f31e25a86f332ca65fdaa0066a24722b2a381abd
dev2820 <second28200@gmail.com>
2023-01-05 18:54:42 +0900

    feature/ relocate broadcast module's source from copy stack repo to here

---
+++ src/utils/createUniqueChannelAddress.ts
@@ -0,0 +1,12 @@
+import CHANNEL_ADDRESS_POSTFIX from "@/constants/CHANNEL_ADDRESS_POSTFIX";
+import ChannelAddress from "@/types/ChannelAddress";
+
+export default function createUniqueChannelAddress(
+  key: string
+): ChannelAddress {
+  const timestamp = Date.now();
+  return {
+    sender: key + timestamp + CHANNEL_ADDRESS_POSTFIX.SENDER,
+    receiver: key + timestamp + CHANNEL_ADDRESS_POSTFIX.RECIVER,
+  };
+}
